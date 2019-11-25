f0b329ba3ddb87d77d6f181da357389aa955278e
splanard-thales <sebastien.planard@muthalesgroup.io>
2019-11-25 17:34:03 +0100

    Implémentation de PaymentFormConfigurationServiceImpl

--- src/main/java/com/payline/payment/equens/service/impl/PaymentFormConfigurationServiceImpl.java
+++ src/main/java/com/payline/payment/equens/service/impl/PaymentFormConfigurationServiceImpl.java
@@ -1,13 +1,23 @@
 package com.payline.payment.equens.service.impl;
 
+import com.payline.payment.equens.bean.business.reachdirectory.Aspsp;
+import com.payline.payment.equens.bean.business.reachdirectory.GetAspspsResponse;
 import com.payline.pmapi.bean.common.FailureCause;
+import com.payline.pmapi.bean.paymentform.bean.field.SelectOption;
+import com.payline.pmapi.bean.paymentform.bean.form.BankTransferForm;
+import com.payline.pmapi.bean.paymentform.bean.form.CustomForm;
 import com.payline.pmapi.bean.paymentform.request.PaymentFormConfigurationRequest;
 import com.payline.pmapi.bean.paymentform.response.configuration.PaymentFormConfigurationResponse;
 import com.payline.pmapi.bean.paymentform.response.configuration.impl.PaymentFormConfigurationResponseFailure;
+import com.payline.pmapi.bean.paymentform.response.configuration.impl.PaymentFormConfigurationResponseSpecific;
 import com.payline.pmapi.logger.LogManager;
 import org.apache.logging.log4j.Logger;
 import com.payline.payment.equens.exception.PluginException;
 import com.payline.payment.equens.service.LogoPaymentFormConfigurationService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class PaymentFormConfigurationServiceImpl extends LogoPaymentFormConfigurationService {
 
@@ -17,8 +27,34 @@ public class PaymentFormConfigurationServiceImpl extends LogoPaymentFormConfigur
     public PaymentFormConfigurationResponse getPaymentFormConfiguration(PaymentFormConfigurationRequest paymentFormConfigurationRequest) {
         PaymentFormConfigurationResponse pfcResponse;
         try {
-            // TODO: build and return payment form
-            pfcResponse = null;
+            Locale locale = paymentFormConfigurationRequest.getLocale();
+
+            // retrieve the banks list from the plugin configuration
+            final List<SelectOption> aspsps = new ArrayList<>();
+            for( Aspsp aspsp : GetAspspsResponse.fromJson( paymentFormConfigurationRequest.getPluginConfiguration() ).getAspsps() ){
+                String value = aspsp.getBic();
+                if( !aspsp.getName().isEmpty() ){
+                    value += " - " + aspsp.getName().get(0);
+                }
+                aspsps.add( SelectOption.SelectOptionBuilder.aSelectOption()
+                        .withKey( aspsp.getAspspId() )
+                        .withValue( value )
+                        .build() );
+            }
+
+            // Build form
+            CustomForm form = BankTransferForm.builder()
+                    .withBanks( aspsps )
+                    .withDescription( i18n.getMessage( "paymentForm.description", locale ) )
+                    .withDisplayButton( true )
+                    .withButtonText( i18n.getMessage( "paymentForm.buttonText", locale ) )
+                    .withCustomFields( new ArrayList<>() )
+                    .build();
+
+            pfcResponse = PaymentFormConfigurationResponseSpecific.PaymentFormConfigurationResponseSpecificBuilder
+                    .aPaymentFormConfigurationResponseSpecific()
+                    .withPaymentForm( form )
+                    .build();
         }
         catch( PluginException e ){
             pfcResponse = e.toPaymentFormConfigurationResponseFailureBuilder().build();
--- src/test/java/com/payline/payment/equens/MockUtils.java
+++ src/test/java/com/payline/payment/equens/MockUtils.java
@@ -334,7 +334,7 @@ public class MockUtils {
      * Generate a valid plugin configuration, as a <code>String</code>.
      */
     public static String aPluginConfiguration(){
-        return "{\"Application\":\"PIS\",\"ASPSP\":[{\"AspspId\":\"7005\",\"BIC\":\"TODO\",\"CountryCode\":\"NL\",\"Name\":[\"Ing Bank\"]}],\"MessageCreateDateTime\":\"2019-11-15T16:52:37.092+0100\",\"MessageId\":\"6f31954f-7ad6-4a63-950c-a2a363488e\"}";
+        return "{\"Application\":\"PIS\",\"ASPSP\":[{\"AspspId\":\"224\",\"BIC\":\"BNPADEFE\",\"CountryCode\":\"DE\",\"Name\":[\"08/15direkt\"]}],\"MessageCreateDateTime\":\"2019-11-15T16:52:37.092+0100\",\"MessageId\":\"6f31954f-7ad6-4a63-950c-a2a363488e\"}";
     }
 
     /**
