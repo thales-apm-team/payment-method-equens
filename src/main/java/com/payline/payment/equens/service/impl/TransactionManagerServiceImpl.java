ccb9c28aef4aa58d4980ebf4a58b9559289291a0
splanard-thales <sebastien.planard@muthalesgroup.io>
2019-11-27 16:47:22 +0100

    Implémentation par défaut de TransactionManagerServiceImpl (renvoie une map vide)

--- src/main/java/com/payline/payment/equens/service/impl/TransactionManagerServiceImpl.java
+++ src/main/java/com/payline/payment/equens/service/impl/TransactionManagerServiceImpl.java
@@ -2,14 +2,15 @@ package com.payline.payment.equens.service.impl;
 
 import com.payline.pmapi.service.TransactionManagerService;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class TransactionManagerServiceImpl implements TransactionManagerService {
 
     @Override
     public Map<String, String> readAdditionalData(String s, String s1) {
-        // TODO
-        return null;
+        // TODO: check if there is a need for transaction additional data
+        return new HashMap<>();
     }
 
 }
