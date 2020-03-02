372d270b9640fce7b285898e84f0e8f1a7d9bcc5
JanFLORENT <41673610+JanFLORENT@users.noreply.github.com>
2020-03-02 15:31:41 +0100

    PAYLAPMEXT-221 Format BIC (#5)
    * PAYLAPMEXT-221 Format BIC + corrections après relecture

--- src/main/java/com/payline/payment/equens/utils/PluginUtils.java
+++ src/main/java/com/payline/payment/equens/utils/PluginUtils.java
@@ -94,8 +94,37 @@ public class PluginUtils {
         return (s == null || s.length() == 0);
     }
 
-
+    /**
+     * Try to find in aspsps list an aspsp with the given BIC11
+     * if no aspsp found, try again by truncating the BIC11 into a BIC8
+     *
+     * @param aspsps The list of aspsp
+     * @param bic    The BIC to find
+     * @return The ID of the aspsp
+     * @see https://payline.atlassian.net/browse/PAYLAPMEXT-221
+     */
     public static String getAspspIdFromBIC(List<Aspsp> aspsps, String bic) {
+        if (isEmpty(bic) || bic.length()<8){
+            throw new InvalidDataException("Invalid bic:" + bic);
+        }
+
+        // try with the full BIC11
+        String aspspId = filter(aspsps, bic);
+
+        // no aspsp for this BIC11, we'll try with the BIC8
+        if (aspspId == null) {
+            aspspId = filter(aspsps, bic.substring(0, 8));
+        }
+
+        if (aspspId == null) {
+            throw new InvalidDataException("Aspsp list does not contain the bic:" + bic);
+        }
+
+        // return its aspspId
+        return aspspId;
+    }
+
+    public static String filter(List<Aspsp> aspsps, String bic) {
         // found aspsp with the correct Bic
         List<Aspsp> goodAspsps = aspsps.stream()
                 .filter(x -> !PluginUtils.isEmpty(x.getBic()))
@@ -103,7 +132,7 @@ public class PluginUtils {
                 .collect(Collectors.toList());
 
         if (goodAspsps.isEmpty()) {
-            throw new InvalidDataException("Aspsp list does not contain the bic:"+ bic);
+            return null;
         }
 
         // return its aspspId
--- src/test/java/com/payline/payment/equens/utils/PluginUtilsTest.java
+++ src/test/java/com/payline/payment/equens/utils/PluginUtilsTest.java
@@ -1,11 +1,16 @@
 package com.payline.payment.equens.utils;
 
+import com.payline.payment.equens.bean.business.reachdirectory.Aspsp;
+import com.payline.payment.equens.bean.business.reachdirectory.GetAspspsResponse;
+import com.payline.payment.equens.exception.PluginException;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -13,14 +18,14 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 class PluginUtilsTest {
 
     @Test
-    void requestToString_get(){
+    void requestToString_get() {
         // given: a HTTP request with headers
-        HttpGet request = new HttpGet( "http://domain.test.fr/endpoint" );
+        HttpGet request = new HttpGet("http://domain.test.fr/endpoint");
         request.setHeader("Authorization", "Basic sensitiveStringThatShouldNotAppear");
         request.setHeader("Other", "This is safe to display");
 
         // when: converting the request to String for display
-        String result = PluginUtils.requestToString( request );
+        String result = PluginUtils.requestToString(request);
 
         // then: the result is as expected
         String ln = System.lineSeparator();
@@ -31,14 +36,14 @@ class PluginUtilsTest {
     }
 
     @Test
-    void requestToString_post(){
+    void requestToString_post() {
         // given: a HTTP request with headers
-        HttpPost request = new HttpPost( "http://domain.test.fr/endpoint" );
+        HttpPost request = new HttpPost("http://domain.test.fr/endpoint");
         request.setHeader("SomeHeader", "Header value");
-        request.setEntity( new StringEntity( "{\"name\":\"Jean Martin\",\"age\":\"28\"}", StandardCharsets.UTF_8 ));
+        request.setEntity(new StringEntity("{\"name\":\"Jean Martin\",\"age\":\"28\"}", StandardCharsets.UTF_8));
 
         // when: converting the request to String for display
-        String result = PluginUtils.requestToString( request );
+        String result = PluginUtils.requestToString(request);
 
         // then: the result is as expected
         String ln = System.lineSeparator();
@@ -54,6 +59,26 @@ class PluginUtilsTest {
         assertEquals("01234567890123456789", PluginUtils.truncate("01234567890123456789", 60));
         assertEquals("", PluginUtils.truncate("", 30));
         assertNull(PluginUtils.truncate(null, 30));
+    }
+
+
+    @Test
+    void getAspspIdFromBIC() {
+        String aspspJson = "{\"Application\":\"PIS\",\"ASPSP\":[" +
+                "{\"AspspId\":\"1234\",\"Name\":[\"a Bank\"],\"CountryCode\":\"FR\",\"BIC\":\"FOOBARBAZXX\"}," +
+                "{\"AspspId\":\"1409\",\"Name\":[\"La Banque Postale\"],\"CountryCode\":\"FR\",\"BIC\":\"PSSTFRPP\"}," +
+                "{\"AspspId\":\"1601\",\"Name\":[\"BBVA\"],\"CountryCode\":\"ES\",\"BIC\":\"BBVAESMM\"}" +
+                "],\"MessageCreateDateTime\":\"2019-11-15T16:52:37.092+0100\",\"MessageId\":\"6f31954f-7ad6-4a63-950c-a2a363488e\"}";
+
+        List<Aspsp> aspsps = GetAspspsResponse.fromJson(aspspJson).getAspsps();
+
+        Assertions.assertEquals("1234", PluginUtils.getAspspIdFromBIC(aspsps, "FOOBARBAZXX"));
+        Assertions.assertEquals("1409", PluginUtils.getAspspIdFromBIC(aspsps, "PSSTFRPP"));
+        Assertions.assertEquals("1409", PluginUtils.getAspspIdFromBIC(aspsps, "PSSTFRPPXXX"));
+        Assertions.assertThrows(PluginException.class, () -> PluginUtils.getAspspIdFromBIC(aspsps, "ABADBIC8"));
+        Assertions.assertThrows(PluginException.class, () -> PluginUtils.getAspspIdFromBIC(aspsps, "BADBIC11"));
+        Assertions.assertThrows(PluginException.class, () -> PluginUtils.getAspspIdFromBIC(aspsps, "BADBIC7"));
+        Assertions.assertThrows(PluginException.class, () -> PluginUtils.getAspspIdFromBIC(aspsps, null));
     }
 
 }
